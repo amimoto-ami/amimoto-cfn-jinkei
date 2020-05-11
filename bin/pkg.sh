3fb2f588b20f91e91f753451a83a07b973403f3a
Hidetaka Okamoto <info@wp-kyoto.net>
2020-05-11 17:19:41 +0900

    update command

--- bin/pkg.sh
+++ bin/pkg.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 git checkout -b release
 ls -a | grep -v -E "^\.$|^\.\.$|^.git$|^cloudformation" | xargs rm -rf
+cp ./cloudformation/*.yaml ./
 git add ./
 git commit -m "create release package"
-echo "Please run npm version [major|minor|patch] and push remote repository by tag"
+echo "Please run git tag [vX.X.X] && git push origin vX.X.X to publish the new version"
