a565e2cdb5ee4cadce565990c2540063ab660c29
Hidetaka Okamoto <info@wp-kyoto.net>
2020-05-11 17:13:00 +0900

    pkg command

--- bin/pkg.sh
+++ bin/pkg.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-ls -a | grep -v -E "^\.$|^\.\.$|^.git$|^cloudformation" 
+git checkout -b release
+ls -a | grep -v -E "^\.$|^\.\.$|^.git$|^cloudformation" | xargs rm -rf
+git add ./
+git commit -m "create release package"
+echo "Please run npm version [major|minor|patch] and push remote repository by tag"
