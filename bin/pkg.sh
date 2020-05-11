c45714dcd1f89f2ae8609e5827c71426718bbc9c
Hidetaka Okamoto <info@wp-kyoto.net>
2020-05-11 17:13:07 +0900

    create release package

--- bin/pkg.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-git checkout -b release
-ls -a | grep -v -E "^\.$|^\.\.$|^.git$|^cloudformation" | xargs rm -rf
-git add ./
-git commit -m "create release package"
-echo "Please run npm version [major|minor|patch] and push remote repository by tag"
--- lib/resources/RDS/model.ts
+++
@@ -1,6 +0,0 @@
-import { Vpc, SecurityGroup } from "@aws-cdk/aws-ec2";
-
-export type RDSDependedResources = {
-    vpc: Vpc;
-    sg: SecurityGroup;
-}
