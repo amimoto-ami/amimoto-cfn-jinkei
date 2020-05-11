b483498072501b6f1c0d549e33d7f8d6b1aec511
Hidetaka Okamoto <info@wp-kyoto.net>
2020-05-11 17:29:08 +0900

    fix pkg cmd

--- bin/pkg.sh
+++ bin/pkg.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-git checkout -b release
+git branch -f release
+git checkout release
 ls -a | grep -v -E "^\.$|^\.\.$|^.git$|^cloudformation" | xargs rm -rf
 cp ./cloudformation/*.yaml ./
 git add ./
