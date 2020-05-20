a2a2ec770ea3694b25b43b3eaca476437bc88754
Hidetaka Okamoto <info@wp-kyoto.net>
2020-05-20 15:06:06 +0900

    create release package

--- lib/resources/helper.ts
+++
@@ -1,9 +0,0 @@
-import { Stack, ScopedAws } from "@aws-cdk/core";
-
-/**
- * Get `Ref: 'AWS::StackName'` props
- * @param stack 
- */
-export const getStackName = (stack: Stack): string => {
-    return new ScopedAws(stack).stackName
-}
