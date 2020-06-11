d3edde49d4a2e21b2c0e56010d4b92024fc11869
Hidetaka Okamoto <info@wp-kyoto.net>
2020-06-11 20:22:56 +0900

    create release package

--- bin/pkg.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-git branch -f release
-git checkout release
-ls -a | grep -v -E "^\.$|^\.\.$|^.git$|^cloudformation" | xargs rm -rf
-cp ./cloudformation/*.yaml ./
-git add ./
-git commit -m "create release package"
-echo "Please run git tag [vX.X.X] && git push origin vX.X.X to publish the new version"
